3cc65bc32bea60b604e0a706f500ed8fe78d5900
saa-hil <workforsahil@gmail.com>
2025-12-08 21:34:40 +0530

    path fixes:

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Elysia, t } from 'elysia';
 import { ImageController } from '../controllers/image.controller';
 
-export const routes = new Elysia({ prefix: '/images' })
+export const routes = new Elysia({ prefix: '/' })
   .get('/:imageId', ImageController.getImage)
   .delete('/:imageId', ImageController.deleteImage);
